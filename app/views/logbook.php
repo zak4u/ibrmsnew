7cc6cc0f224fb255f0bd7493bf426f92f2ae27c6
Lyndon <lyndon.astorga@gmail.com>
2017-11-09 14:27:53 +0800

    nov9

--- app/views/logbook.php
+++ app/views/logbook.php
@@ -1,32 +1,35 @@
 <div class="row">
-  <div class="col-lg-12">
-    <table id="dataTableResident" class="table table-striped table-bordered dataTable" cellspacing="0" width="100%">
-      <thead>
-        <tr>
-          <th width="20%">Date/Time</th>
-          <th width="20%">User Name</th>
-          <th width="30%">Details</th>
-        </tr>
-      </thead>
-      <tfoot>
-        <tr>
-          <th>Date/Time</th>
-          <th>User Name</th>
-          <th>Details</th>
-        </tr>
-      </tfoot>
-      <tbody>
-        <tr>
-          <td>23 May 2014 03:08:03am </td>
-          <td>Lyndon Astorga</td>
-          <td>logged in</td>
-        </tr>
-        <tr>
-          <td>23 May 2014 03:08:03am </td>
-          <td>Lyndon Astorga</td>
-          <td>added Resident ID 17-1234567</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <div class="col-lg-12">
+        <div class="box box-solid">
+            <div class="box-body">
+                <table id="dataTableResident" class="table table-striped table-bordered dataTable" cellspacing="0" width="100%">
+                    <thead>
+                        <tr>
+                            <th width="20%">Date/Time</th>
+                            <th width="20%">User Name</th>
+                            <th width="30%">Details</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>Date/Time</th>
+                            <th>User Name</th>
+                            <th>Details</th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        <tr>
+                            <td>23 May 2014 03:08:03am </td>
+                            <td>Lyndon Astorga</td>
+                            <td>logged in</td>
+                        </tr>
+                        <tr>
+                            <td>23 May 2014 03:08:03am </td>
+                            <td>Lyndon Astorga</td>
+                            <td>added Resident ID 17-1234567</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div> </div>
 </div>
